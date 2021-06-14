b364f9a8a88c0bb7630007643bd7ed3b7109ab25
nayananga <nayanangamuhandiram@gmail.com>
2021-06-14 23:10:36 +0530

    Updated Config.py to run docker tests

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 NUMBER_OF_ITERATIONS = 200
 NUMBER_OF_TRAINING_POINTS = 30
-EVAL_POINT_SIZE = 1000
+EVAL_POINT_SIZE = 50
 DEFAULT_TRADE_OFF_LEVEL = 0.1
 PAUSE_TIME = 0.1
 
@@ -12,26 +12,22 @@ PARAMETERS = ['para_1']
 FEATURE = ['feature_1']
 
 # PARAMETER BOUNDS
-PARAMETER_BOUNDS = [[1, 1000]]
+PARAMETER_BOUNDS = [[1, 50]]
 
 # PARAMETER BOUNDS
-FEATURE_BOUNDS = [[1, 500]]
+FEATURE_BOUNDS = [[1, 50]]
 
 # To define a new function go to simulation_utilities/data_generations/feature_generator.py
-# FEATURE_FUNCTION_ARRAY = [['INCREASE_AND_DECREASE_FUNCTION'], ['ONE_STEP_FUNCTION'], ['CONSTANT'], ['PEAKS'],
-#                           ['RANDOM'], ['STEP_INCREASE_FUNCTION'], ['UP_AND_DOWN_FUNCTION']]
-
 FEATURE_FUNCTION_ARRAY = [['ACTUAL']]
 
-# FILE_NAME = ['Increase_and_decrease', 'One_step', 'Constant', 'Peaks', 'Random', 'Step_increase', 'Up_and_down']
 FILE_NAME = ['ACTUAL']
 
 # FUNCTION = "((p1-f1)^2)/20+(f1^2/1000)"
 FUNCTION = "0.000002*(p1 - f1)^4 - 0.00091*(p1 - f1) ^ 3 + 0.123*(p1 - f1) ^ 2 - 4.8411*(p1 - f1)+200+ (f1 ^ 2)/1000"
 
-ROOT_PATH = '/home/nayananga/PycharmProjects/AI-BasedThreadPoolTuningModel/Data/'
+ROOT_PATH = '/app/Data/'
 
-TRAINING_DATA_PATH = '/home/nayananga/Desktop/jmeter_service/python_service_train_data'
+TRAINING_DATA_PATH = '/app/Data/Training_data'
 TEST_NAME = None
 
 FUNCTION_NAME = 'Function_2'
